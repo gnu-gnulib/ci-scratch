90647358f613056acbdaca4c0feda531fddf3176
Bruno Haible <bruno@clisp.org>
2024-05-21 21:59:11 +0200

    Template for scratch work.

---
+++ build-on.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Copyright (C) 2024 Free Software Foundation, Inc.
+#
+# This file is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# This script builds a tarball of the package on a single platform.
+# Usage: build-on.sh PACKAGE CONFIGURE_OPTIONS MAKE PREFIX PREREQUISITES
+
+package="$1"
+configure_options="$2"
+make="$3"
+prefix="$4"
+prerequisites="$5"
+
+set -x
+
+# Build and install the prerequisites.
+for prereq in $prerequisites; do
+  tar xfz $prereq.tar.gz
+  cd $prereq
+  ./configure $configure_options --prefix="$prefix" > log1 2>&1; rc=$?; cat log1; test $rc = 0 || exit 1
+  $make > log2 2>&1; rc=$?; cat log2; test $rc = 0 || exit 1
+  $make install > log4 2>&1; rc=$?; cat log4; test $rc = 0 || exit 1
+  cd ..
+done
+
+# We expect to find the sources in the repository.
+cd testdir1 || exit 1
+
+mkdir build
+cd build
+
+# Configure.
+CPPFLAGS="$CPPFLAGS -DCONTINUE_AFTER_ASSERT" \
+FORCE_UNSAFE_CONFIGURE=1 ../configure --config-cache --with-included-libunistring $configure_options > log1 2>&1; rc=$?; cat log1; test $rc = 0 || exit 1
+
+# Build.
+$make > log2 2>&1; rc=$?; cat log2; test $rc = 0 || exit 1
+
+# Run the tests.
+$make check > log3 2>&1; rc=$?; cat log3; test $rc = 0 || exit 1
+
+cd ..
