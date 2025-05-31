137658852f3f34f9fbef2658233e2839cf0fbe18
Bruno Haible <bruno@clisp.org>
2025-05-31 23:44:48 +0200

    testdir22: try 1.

--- build-on.sh
+++ build-on.sh
@@ -65,7 +65,8 @@ done
 # testdir19: gettext-runtime
 # testdir20: stddef-h
 # testdir21: open openat
-cd testdir20 || exit 1
+# testdir22: stat-time
+cd testdir22 || exit 1
 
 # Bring the time stamps into an order that will not require autoconf, automake, etc. to run again.
 sleep 1; touch `find . -name aclocal.m4 -type f`
