1526d13883ccab62e9e3f0870a7d6ac1dba84ef1
Bruno Haible <bruno@clisp.org>
2025-08-14 21:50:13 +0200

    testdir28: try 1.

--- build-on.sh
+++ build-on.sh
@@ -71,7 +71,8 @@ done
 # testdir25: assert-h
 # testdir26: all
 # testdir27: nstrftime
-cd testdir27 || exit 1
+# testdir28: issymlink, issymlinkat
+cd testdir28 || exit 1
 
 # Bring the time stamps into an order that will not require autoconf, automake, etc. to run again.
 sleep 1; touch `find . -name aclocal.m4 -type f`
