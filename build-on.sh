b24ccd46f9a563f785cc8496b0629c11789e6ec9
Bruno Haible <bruno@clisp.org>
2026-05-06 13:34:50 +0200

    testdir48: try 1.

--- build-on.sh
+++ build-on.sh
@@ -91,7 +91,8 @@ done
 # testdir45: regex
 # testdir46: xstrtol
 # testdir47: regex
-cd testdir47 || exit 1
+# testdir48: gzip-1.14.70
+cd testdir48 || exit 1
 
 # Bring the time stamps into an order that will not require autoconf, automake, etc. to run again.
 sleep 1; touch `find . -name aclocal.m4 -type f`
