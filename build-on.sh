af0ab536f5c058bf841d135fa33ad961d9e2d897
Bruno Haible <bruno@clisp.org>
2026-08-08 00:33:22 +0200

    testdir62: try 1.

--- build-on.sh
+++ build-on.sh
@@ -105,7 +105,8 @@ done
 # testdir59: list sublist list-c++
 # testdir60: hashcode-mem ...
 # testdir61: sd-dlopen
-cd testdir61 || exit 1
+# testdir62: array-omap array-oset hash-map linkedhash-map modechange acl-permissions file-has-acl dfa dirent-h alphasort
+cd testdir62 || exit 1
 
 # Bring the time stamps into an order that will not require autoconf, automake, etc. to run again.
 sleep 1; touch `find . -name aclocal.m4 -type f`
